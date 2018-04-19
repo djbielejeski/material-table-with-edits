452bdaea516cf256404cc40184244aca50843187
djbielejeski <djbielejeski@gmail.com>
2018-04-19 09:14:57 -0500

    DB: Updating the routes to default "/" instead of "/home"

--- src/app/core/routes.ts
+++ src/app/core/routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '@app/home/home.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   // Wildcard route
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
